0e165800584263c2fd10f238a8e7f1abcc1eec80
faizaST <faiza.tiaramadhan@gmail.com>
2025-06-25 13:11:59 +0700

    yaml

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ void main() async {
 
   await Supabase.initialize(
     url:
-        'https://bydjzlkudumkqqswfglh.supabase.co', // Ganti dengan project Supabase kamu
+        'https://bydjzlkudumkqqswfglh.supabase.coo', // Ganti dengan project Supabase kamu
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5ZGp6bGt1ZHVta3Fxc3dmZ2xoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1OTQzOTEsImV4cCI6MjA2NjE3MDM5MX0.JKsYo86nES8jGnJOpbW5qMe0gAK7f9zAo3-ZihN8uo4', // Ganti juga dengan anon key kamu
   );
